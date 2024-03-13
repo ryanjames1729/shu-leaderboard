e622c2b87a383680316b76f57bccbdf78f8a1cfc
Ryan James <ryanjames1729@gmail.com>
2024-03-13 10:50:55 -0400

    adjusting middleware restrictions for auth

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,11 @@
+
+ 
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    ignoredRoutes: ['/']
+  publicRoutes: ["/"],
 });
+ 
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
